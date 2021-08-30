ae30f212c6639b2cb4e30243824c6f4025741d13
MielomankA <mielomanka@mail.ru>
2021-08-31 00:45:54 +0300

    feat: add part solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,48 @@
+const firstPlayer = 'x';
+const secondPlayer = 'o';
+/* const nextPlayer = {
+  [firstPlayer]: secondPlayer,
+  [secondPlayer]: firstPlayer,
+}; */
+
 class TicTacToe {
-    constructor() {
+  currentPlayer = firstPlayer;
+  matrix = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  constructor() {}
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.matrix[rowIndex][columnIndex]) {
+      return;
     }
+    this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.noMoreTurns();
+  }
 
-    }
+  getWinner() {}
 
-    nextTurn(rowIndex, columnIndex) {
+  noMoreTurns() {
+    return this.matrix.every((row) => row.every((col) => col));
+  }
 
-    }
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner() ? true : false;
+  }
 
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.matrix[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
